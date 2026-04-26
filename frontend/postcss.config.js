26ac99c0db916b75cc259f3f5f4b8020079c7395
AyushJaswal420 <jaswalshahab@gmail.com>
2026-04-26 23:53:20 +0530

    done

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // Add the /postcss part here
+    'autoprefixer': {},
   },
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import Layout from "./components/Layout";
